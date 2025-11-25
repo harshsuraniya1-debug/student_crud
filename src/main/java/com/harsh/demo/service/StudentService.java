78b952a8f8ecf17619189804f991f47a70fda500
harshsuraniya1-debug <harshsuraniya1@gmail.com>
2025-11-25 22:41:00 +0530

    Added endpoints for post,put,delete and delete all

--- src/main/java/com/harsh/demo/service/StudentService.java
+++ src/main/java/com/harsh/demo/service/StudentService.java
@@ -27,8 +27,54 @@ public class StudentService {
         return  studentDtoList;
 
     }
-//    public StudentDto getStudentById (long id){
-//       Optional<Student> student = studentRepository.findById(id);
-//       MapperUtil.toDto(student)
-//    }
+    public StudentDto getStudentById (long id){
+       Optional<Student> student = studentRepository.findById(id);
+
+       if(student.isPresent()){
+           return MapperUtil.toDto(student.get());
+       }
+
+       throw new RuntimeException("Student with given id is not found ");
+
+    }
+
+   public void deleteAllStudents(){
+        studentRepository.deleteAll();
+   }
+
+   public void deleteStudent(long id){
+        studentRepository.deleteById(id);
+   }
+
+   public StudentDto createStudent(StudentDto studentDto){
+
+       Student student = MapperUtil.toEntity(studentDto);
+
+       Student savedStudent = studentRepository.save(student);
+
+       return MapperUtil.toDto(savedStudent);
+
+   }
+
+   public StudentDto updateStudent(StudentDto studentDto,long id){
+
+        Optional<Student> existingstudent = studentRepository.findById(id);
+
+        if(existingstudent.isPresent()){
+
+            Student optionalstudent = existingstudent.get();
+            optionalstudent.setRollNo(studentDto.getRollNo());
+            optionalstudent.setState(studentDto.getState());
+            optionalstudent.setSurname(studentDto.getSurname());
+            optionalstudent.setPhone(studentDto.getPhone());
+            optionalstudent.setName(studentDto.getSurname());
+
+            Student updatedstudent = studentRepository.save(optionalstudent);
+            return MapperUtil.toDto(updatedstudent);
+        }
+        else{
+            throw new RuntimeException("Student id is not found : " + id);
+        }
+
+   }
 }
