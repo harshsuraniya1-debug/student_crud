f1e31ab317dafe75114e53ba3b975f6da5702a51
harshsuraniya1-debug <harshsuraniya1@gmail.com>
2025-11-25 23:01:56 +0530

    changes for error in the post request

--- src/main/java/com/harsh/demo/mapper/MapperUtil.java
+++ src/main/java/com/harsh/demo/mapper/MapperUtil.java
@@ -7,7 +7,7 @@ public class MapperUtil {
 
     public static Student toEntity(StudentDto studentDto){
         Student student = new Student();
-        student.setId(studentDto.getId());
+        //student.setId(studentDto.getId());
         student.setName(studentDto.getName());
         student.setState(studentDto.getState());
         student.setSurname(studentDto.getSurname());
@@ -20,7 +20,7 @@ public class MapperUtil {
 
     public static StudentDto toDto(Student student){
         StudentDto studentDto = new StudentDto();
-        studentDto.setId(student.getId());
+        //studentDto.setId(student.getId());
         studentDto.setName(student.getName());
         studentDto.setPhone(student.getPhone());
         studentDto.setState(student.getState());
